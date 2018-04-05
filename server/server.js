428f2f2e5aec2e335a71f3681ea98be91abe1732
Alon <3alonb@gmail.com>
2018-04-05 15:00:04 +0300

    changes

--- server/server.js
+++ server/server.js
@@ -1,12 +1,25 @@
 const path = require('path');
+const http = require('http');
 const express = require('express');
+const socketIO = require('socket.io');
 
 const publicPath = path.join(__dirname, '../public');
 
 var app = express();
+var server = http.createServer(app);
+var io = socketIO(server);
+
+io.on('connection', (socket) => {
+  console.log('new user connected');
+
+  socket.on('disconnect', () => {
+    console.log('user disconnected');
+  });
+
+});
 
 app.use(express.static(publicPath));
 
-app.listen(3000, () => {
+server.listen(3000, () => {
   console.log('server running on port 3000');
 });
