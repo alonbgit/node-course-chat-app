7a31797c91a2b1ac410f4fc916689dc3f8b253e0
Alon <3alonb@gmail.com>
2018-04-06 13:44:20 +0300

    changes

--- public/js/index.js
+++ public/js/index.js
@@ -7,4 +7,26 @@ socket.on('connect', function() {
 
 socket.on('newMessage', function(message) {
   console.log('New message received from server', message);
+  var li = $('<li></li>').text(`${message.from}: ${message.text}`);
+  $('#messages').append(li);
+});
+
+socket.emit('createMessage', {
+    from: 'Frank',
+    text: 'hi'
+  }, function(data) {
+    console.log('got it', data
+  );
+});
+
+$('#message-form').on('submit', function(e) {
+  e.preventDefault();
+
+  socket.emit('createMessage', {
+    from: 'User',
+    text: $('[name=message]').val()
+  }, function() {
+
+  });
+
 });
